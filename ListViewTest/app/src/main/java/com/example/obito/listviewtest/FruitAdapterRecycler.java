26d2a73a08df22a7307d59533a4d51defa4a3619
MSObito <1004965416@qq.com>
2017-12-18 18:01:03 +0800

    third commit

--- ListViewTest/app/src/main/java/com/example/obito/listviewtest/FruitAdapterRecycler.java
+++ ListViewTest/app/src/main/java/com/example/obito/listviewtest/FruitAdapterRecycler.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,9 +16,22 @@ import java.util.List;
  * Created by obito on 2017/12/14.
  */
 
-public class FruitAdapterRecycler extends RecyclerView.Adapter<FruitAdapterRecycler.ViewHolder> {
+public class FruitAdapterRecycler extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Fruit> mFruitList;
+
+    private static final int TYPE_ITEM   = 0;
+    private static final int TYPE_FOOTER = 1;
+
+    //上拉加载更多
+    public static final int PULLUP_LOAD_MORE = 0;
+    //正在加载中
+    public static final int LOADING_MORE     = 1;
+    //没有加载更多 隐藏
+    public static final int NO_LOAD_MORE     = 2;
+
+    //上拉加载更多状态-默认为0
+    private int mLoadMoreStatus = 0;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView fruitImage;
@@ -27,8 +42,24 @@ public class FruitAdapterRecycler extends RecyclerView.Adapter<FruitAdapterRecyc
         public ViewHolder(View view){
             super(view);
             fruitView=view;
-            fruitImage=(ImageView)view.findViewById(R.id.fruit_image2);
-            fruitName=(TextView)view.findViewById(R.id.fruit_name2);
+            fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
+            fruitName=(TextView)view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    static class FooterViewHolder extends RecyclerView.ViewHolder{
+        ProgressBar progressBar;
+        TextView loadTextView;
+        LinearLayout loadLinearLayout;
+
+        View footerView;
+
+        public FooterViewHolder(View view){
+            super(view);
+            footerView=view;
+            progressBar=(ProgressBar)footerView.findViewById(R.id.pbLoad);
+            loadTextView=(TextView)footerView.findViewById(R.id.tvLoadText);
+            loadLinearLayout=(LinearLayout)footerView.findViewById(R.id.load_more_layout);
         }
     }
 
@@ -36,42 +67,98 @@ public class FruitAdapterRecycler extends RecyclerView.Adapter<FruitAdapterRecyc
         mFruitList=fruitList;
     }
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item_staggered,parent,false);
-        final ViewHolder holder=new ViewHolder(view);
-        holder.fruitView.setOnClickListener(new View.OnClickListener(){
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(viewType==TYPE_ITEM){
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
+            final ViewHolder holder=new ViewHolder(view);
+            holder.fruitView.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                int position=holder.getAdapterPosition();
-                Fruit fruit=mFruitList.get(position);
-                Toast.makeText(v.getContext(),"you clicked view"+fruit.getName(), Toast.LENGTH_SHORT).show();
-            }
-        });
+                @Override
+                public void onClick(View v) {
+                    int position=holder.getAdapterPosition();
+                    Fruit fruit=mFruitList.get(position);
+                    Toast.makeText(v.getContext(),"you clicked view"+fruit.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        holder.fruitImage.setOnClickListener(new View.OnClickListener(){
+            holder.fruitImage.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                int position=holder.getAdapterPosition();
-                Fruit fruit=mFruitList.get(position);
+                @Override
+                public void onClick(View v) {
+                    int position=holder.getAdapterPosition();
+                    Fruit fruit=mFruitList.get(position);
 
-                Toast.makeText(v.getContext(),"you clicked is image!",Toast.LENGTH_SHORT).show();
-            }
-        });
-        return holder;
+                    Toast.makeText(v.getContext(),"you clicked is image!",Toast.LENGTH_SHORT).show();
+                }
+            });
+            return holder;
+        }
+        else if(viewType==TYPE_FOOTER){
+            View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.load_more,parent,false);
+            final FooterViewHolder footerViewHolder=new FooterViewHolder(view);
+            return footerViewHolder;
+        }
+            return null;
     }
+
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Fruit fruit=mFruitList.get(position);
-        holder.fruitImage.setImageResource(fruit.getImageId());
-        holder.fruitName.setText(fruit.getName());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ViewHolder) {
+            ViewHolder viewHolder=(ViewHolder)holder;
+            Fruit fruit = mFruitList.get(position);
+            viewHolder.fruitImage.setImageResource(fruit.getImageId());
+            viewHolder.fruitName.setText(fruit.getName());
+        } else if (holder instanceof FooterViewHolder) {
+            FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
+            switch (mLoadMoreStatus) {
+                case PULLUP_LOAD_MORE:
+                    footerViewHolder.loadTextView.setText("上拉加载更多...");
+                    break;
+                case LOADING_MORE:
+                    footerViewHolder.loadTextView.setText("正加载更多...");
+                    break;
+                case NO_LOAD_MORE:
+                    //隐藏加载更多
+                    footerViewHolder.loadLinearLayout.setVisibility(View.GONE);
+                    break;
+            }
+        }
     }
-
 
     @Override
     public int getItemCount() {
-        return mFruitList.size();
+        return mFruitList.size()+1;
     }
+
+    @Override
+    public int getItemViewType(int position){
+        if (position + 1 == getItemCount()) {
+            //最后一个item设置为footerView
+            return TYPE_FOOTER;
+        } else {
+            return TYPE_ITEM;
+        }
+    }
+
+    public void AddHeaderItem(List<Fruit> items){
+        mFruitList.addAll(0,items);
+        notifyDataSetChanged();
+    }
+
+    public void AddFooterItem(List<Fruit> items){
+        mFruitList.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 更新加载更多状态
+     * @param status
+     */
+    public void changeMoreStatus(int status){
+        mLoadMoreStatus=status;
+        notifyDataSetChanged();
+    }
+
+
 }
--- ListViewTest/app/src/main/java/com/example/obito/listviewtest/RecyclerViewActivity.java
+++ ListViewTest/app/src/main/java/com/example/obito/listviewtest/RecyclerViewActivity.java
@@ -1,19 +1,29 @@
 package com.example.obito.listviewtest;
 
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.widget.Toast;
 
 import com.example.obito.model.FruitModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class RecyclerViewActivity extends AppCompatActivity {
 
     private List<Fruit> fruitList=new ArrayList<>();
+
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView recyclerView;
+
+    FruitAdapterRecycler adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +32,99 @@ public class RecyclerViewActivity extends AppCompatActivity {
 
         FruitModel fruitModel=new FruitModel();
         fruitList=fruitModel.GetFruitList();
-        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
-        //LinearLayoutManager layoutManager=new LinearLayoutManager(this);
-        StaggeredGridLayoutManager layoutManager=new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
+        recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
+        mSwipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swipeRefreshLayout);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        //StaggeredGridLayoutManager layoutManager=new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
-        FruitAdapterRecycler adapter=new FruitAdapterRecycler(fruitList);
+        adapter=new FruitAdapterRecycler(fruitList);
         recyclerView.setAdapter(adapter);
+        initListener();
     }
+
+    private void initListener() {
+
+        initPullRefresh();
+
+        initLoadMoreListener();
+
+    }
+
+    private void initPullRefresh() {
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<Fruit> headDatas = new ArrayList<Fruit>();
+                        for (int i = 0; i <10 ; i++) {
+                            Fruit fruit=new Fruit("addApple",R.drawable.apple_pic);
+                            headDatas.add(fruit);
+                        }
+                        adapter.AddHeaderItem(headDatas);
+
+                        //刷新完成
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        Toast.makeText(RecyclerViewActivity.this, "更新了 "+headDatas.size()+" 条目数据", Toast.LENGTH_SHORT).show();
+                    }
+
+                }, 3000);
+
+            }
+        });
+    }
+
+    private void initLoadMoreListener() {
+
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int lastVisibleItem ;
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                //判断RecyclerView的状态 是空闲时，同时，是最后一个可见的ITEM时才加载
+                if(newState==RecyclerView.SCROLL_STATE_IDLE&&lastVisibleItem+1==adapter.getItemCount()){
+
+                    //设置正在加载更多
+                    adapter.changeMoreStatus(adapter.LOADING_MORE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            List<Fruit> footerDatas = new ArrayList<Fruit>();
+                            for (int i = 0; i< 10; i++) {
+                                Fruit fruit=new Fruit("addBanana",R.drawable.banana_pic);
+                                footerDatas.add(fruit);
+                            }
+                            adapter.AddFooterItem(footerDatas);
+                            //设置回到上拉加载更多
+                            adapter.changeMoreStatus(adapter.PULLUP_LOAD_MORE);
+
+                            Toast.makeText(RecyclerViewActivity.this, "更新了 "+footerDatas.size()+" 条目数据", Toast.LENGTH_SHORT).show();
+                        }
+                    }, 3000);
+
+
+                }
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                //最后一个可见的ITEM
+                lastVisibleItem=layoutManager.findLastVisibleItemPosition();
+            }
+        });
+
+    }
+
+
 }
