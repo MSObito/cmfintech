1163f8ef93615a11d12a63dbf66a4b822311b249
MSObito <1004965416@qq.com>
2018-01-08 10:05:40 +0800

    fourth week

---
+++ WebViewTest/app/src/main/java/com/example/obito/webviewtest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.obito.webviewtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=(WebView)findViewById(R.id.web_view);
+        Button button=(Button)findViewById(R.id.second_activity);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.bing.com");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
